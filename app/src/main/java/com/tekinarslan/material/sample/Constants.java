b2fd97c3bdb02a44972f4d2efc183a8a335019ad
Ambrish Tiwari <tiwari.ambrish1@gmail.com>
2016-03-14 18:25:24 +0530

    Update Constants.java

--- app/src/main/java/com/tekinarslan/material/sample/Constants.java
+++ app/src/main/java/com/tekinarslan/material/sample/Constants.java
@@ -4,7 +4,7 @@ package com.tekinarslan.material.sample;
  * Created by IGNATIUZ 2 on 3/9/2016.
  */
 public class Constants {
-    public static final String CAPABILITY_TOKEN_URL = "http://callingapp.ignatiuz.com/AndroidApp/AndroidCapToken";
-    public static final String FROM_NUMBER  = "+18563515180"; //Twilio Number
-    public static final String TO_NUMBER  = "+14844859149";
+    public static final String CAPABILITY_TOKEN_URL = "http://callingapp.domain_name.com/AndroidApp/AndroidCapToken";
+    public static final String FROM_NUMBER  = "Enter your Twilio number"; // Twilio Number
+    public static final String TO_NUMBER  = "Enter the number you wont to call";
 }
